02fe112d6cd6624d7dac8aea34963ef858715645
VERGER--DOUCY BRICE <114079055+BgameB@users.noreply.github.com>
2022-12-01 11:08:10 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -114,3 +114,104 @@ function parite(){
     else
     t4.value = ("Impair")
 }
+
+function affichagetab(){
+    document.write('<table border=2px width=30%');
+    for(let i = 0 ; i <= 5; i++){
+        document.write('<tr><td>*</td><td>*</td><td>*</td></tr>');
+    }
+    document.write('</table>');
+}
+
+function affichagetab2(){
+    var count = prompt("Combien de lignes souhaitez vous ?");
+    console.log(count)
+    document.write('<table border=2px width=30%');
+    for(let i = 1 ; i <= count; i++){
+        document.write(`<tr><td>${i}</td><td>*</td><td>*</td></tr>`);
+    }
+    document.write('</table>');
+
+}
+
+function seconnecter(){
+    var login = prompt("Donner votre nom d'utilisateur")
+    var password = prompt("Entrer votre mot de passe")
+    if( login == "admin" && password == "admin"){
+        document.write(`Bienvenue :     ${login}`)
+    }
+    else{
+        alert("Accès refusé")
+    }
+}
+
+function seconnecter2(){
+    var i = 0;
+    do {
+    var id = prompt("Donner votre nom d'utilisateur");
+    var mdp = prompt("Entrez votre mot de passe");
+    if (id == "admin" && mdp == "admin")
+    {
+        document.write("Bienvenue :    "+id);
+        break;
+    }
+    else
+    alert("Accès refusé");
+    i+=1;
+    } while (i<3);
+    if (i==3)
+    alert("Délai dépassé");
+}
+function cdc() {
+    var chaine = prompt("Donnez un mot");
+    document.write(chaine.toUpperCase()+"<br>");
+    document.write(chaine.toLowerCase()+"<br>");
+    document.write("a chaine contient    " + chaine.length+"    caractères    " + "<br>");
+    document.write("la première lettre est   " + chaine.substr(0,1)+ "<br>");
+}
+
+function swich() {
+    var color = prompt ("Entrez une couleur");
+    switch(color)
+    {
+        case "rouge" : document.body.style.background = "red";
+        break;
+        case "ROUGE" : document.body.style.background = "red";
+        break;
+        case "bleu" : document.body.style.background = "blue";
+        break;
+        case "BLEU" : document.body.style.background = "blue";
+        break;
+        case "vert" : document.body.style.background = "green";
+        break;
+        case "VERT" : document.body.style.background = "green";
+        break;
+        default:alert("couleur non définie");
+    }
+}
+
+function bonus(){
+
+    var totalf = 0
+    var total = 0
+    var liste = 0
+    var c = ""
+    do{
+        var article = prompt("Votre article")
+        var price = prompt("Le prix")
+        var quantity = prompt("La quantité")
+
+        alert(`Vos ${article} couterons ${price * quantity} €`)
+        total = Number(price)*Number(quantity) 
+        totalf += Number(price)*Number(quantity) 
+        c += ("Articles : " +article+" <br> PRIX : "+price + " <br> Quantité : "+quantity+ " <br> Total : "+total+"<br><br><br>")
+        alert('Vos articles additionnés courterons ' + totalf + "€")
+
+        next = prompt("Voulez vous continuer (OUI/STOP) ?")
+    }
+    while(next != "STOP");
+    document.write(c);
+    document.write(" TOTAL FINAL : "+totalf);
+
+
+}
